72751feada6da19bb3b1b48c3b8fe52713f4fb4c
KwonSunJae <32411719+KwonSunJae@users.noreply.github.com>
2022-10-08 16:54:35 +0900

    [SHELL] UPDATE progress bar

--- init.sh
+++ init.sh
@@ -1,11 +1,32 @@
 #! /bin/bash
+
+function spinner() {
+    local info="$1"
+    local pid=$!
+    local delay=0.75
+    local spinstr='|/-\'
+    while kill -0 $pid 2> /dev/null; do
+        local temp=${spinstr#?}
+        printf " [%c]  $info" "$spinstr"
+        local spinstr=$temp${spinstr%"$temp"}
+        sleep $delay
+        local reset="\b\b\b\b\b\b"
+        for ((i=1; i<=$(echo $info | wc -c); i++)); do
+            reset+="\b"
+        done
+        printf $reset
+    done
+    printf "    \b\b\b\b"
+}
+
 echo "-----Initalizing r2sh-book-----"
 echo "-----Install essential packages-----"
 
-sudo apt-get update -y -qq
-sudo apt-get install gcc-9 g++-9 -y -qq
+sudo apt-get update -y -qq && spinner "apt updating..."
+sudo apt-get install gcc-9 g++-9 -y -qq && spinner "apt install gcc"
 gcc --version
-sudo apt-get install git -y -qq
+sudo apt-get install git -y -qq spinner " apt install git check"
+
 
 echo "-----Alias Settings-----"
 chmod 777 main.sh
