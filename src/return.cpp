5e6b54340d7c448c7e663e78e6e2a877613300e0
woo4826 <woo4826@gmail.com>
2022-10-21 18:59:50 +0900

    return 제작중

--- src/return.cpp
+++ src/return.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <fstream>
 #include "json/json.h"
-#include <string.h>
+#include <string>
 #include <ctime>
+#include <stdio.h>
+#include <time.h>
 
 using namespace std;
 
@@ -18,7 +20,7 @@ public:
         filedata = filepath; //대상 파일임
     }
 
-    Json::Value Read()
+    Json::Value Read(string key)
     { // 함수 리턴을 객체로 하고 싶으면 원하는대로 수정하면됨. 파일 읽는 예제 코드임.
         ifs = ifstream(filedata);
         Json::CharReaderBuilder builder;
@@ -33,50 +35,13 @@ public:
         }
 
         //값을 뽑는 방법
-        return root["r2shs"];
-    }
-
-    void makeUser(string uId, string uName)
-    { // 똑같이 여기서는 json file 쓰고 수정해서 원래 파일로 넣는 함수임. 원하는 대로 수정 ㄲ.
-        //여기 함수에서는Users에 User 하나를 추가하는 함수임.
-        ofs.open(filedata);
-        Json::Value users; // 여기다 user 계속 추가
-
-        Json::Value user; // 여기가 객체 하나를 계속 작성하는거
-        user["uId"] = uId;
-        user["uName"] = uName;
-
-        //속성중에 배열이 있으면 따로 할당해서 해야됨
-        Json::Value urshes;
-        urshes.append("1");
-        urshes.append("2");
-        urshes.append("3");
-
-        user["uR2shs"] = urshes; //배열은 이런식으로 추가
-        user["uPenalty"] = "0000-00-00";
-
-        users.append(user); //리스트에 넣어주고 아래에서
-
-        root["users"] = users; // 여기서 마무리~
-        //  파일 쓸때 형식임 건들 ㄴㄴ
-        Json::StreamWriterBuilder builder;
-        builder["commentStyle"] = "None";
-        builder["indentation"] = "    "; // Tab
-        unique_ptr<Json::StreamWriter> writer(builder.newStreamWriter());
-        ///////////////////////////////////////
-
-        // 알파벳 순으로 write 된다.
-        // writer->write(root, &cout); cout 출력하고 싶을때
-        writer->write(root, &ofs); // 여기다가 root를 쓰는거임.
-        // cout << endl;  // add lf and flush
-
-        ofs.close();
+        return root[key];
     }
 
     void Write(Json::Value r2shs)
     {
         ofs.open(filedata);
-
+        // cout << r2shs << endl;
         root["r2shs"] = r2shs; // 여기서 마무리~
         //  파일 쓸때 형식임 건들 ㄴㄴ
         Json::StreamWriterBuilder builder;
@@ -94,61 +59,108 @@ public:
     }
 };
 bool search_book_present(string rBId, Json::Value r2shs);
-void return_book(string rBId, Json::Value r2shs);
 
-bool search_book_present(string rBId, Json::Value r2shs)
+string get_today()
+{
+    time_t timer;
+    struct tm *t;
+    timer = time(NULL);
+    t = localtime(&timer);
+    time_t time = mktime(t);
+    cout << time << endl;
+
+    return to_string(t->tm_year + 1900) + "-" + to_string(t->tm_mon + 1) + "-" + to_string(t->tm_mday);
+}
+int find_book(string rBId, Json::Value r2shs)
 {
     for (int i = 0; i < r2shs.size(); i++)
     {
-        if (r2shs[i]["rBId"] == rBId)
+        if (r2shs[i]["rBId"] == rBId && r2shs[i]["rDate"] == "0000-00-00") // bid 가 존재하며, 반납되지 않은책 찾음.
         {
-            cout << "book rush found" << endl;
-            cout << "book name: " << r2shs[i]["rUid"] << r2shs[i]["rDeadline"] << endl;
-            return true;
+            return i;
         }
     }
-    return false;
+    return -1;
 }
-
-void return_book(string rBId, Json::Value r2shs)
+string get_penalty_date(string deadline)
 {
-    if (search_book_present(rBId, r2shs))
-    {
-        //리턴 수행
-        for (int i = 0; i < r2shs.size(); i++)
-        {
-            if (r2shs[i]["rBId"] == rBId && r2shs[i]["rDate"] == "0000-00-00") // bid 가 존재하며, 반납되지 않은책 찾음.
-            {
-                time_t timer;
-                struct tm t;
-                timer = time(NULL);
-                localtime_s(&t, &timer);
-                string date = to_string(t.tm_year + 1900);
-                date += "-";
-                date.append(to_string(t.tm_mon + 1));
-                date += "-";
-                date.append(to_string(t.tm_mday));
-                r2shs[i]["rDate"] = date;
-                //연체여부판단.
-                cout << "book returned" << endl;
-                cout << "book name: " << r2shs[i]["rUid"] << "rDate:" << r2shs[i]["rDate"] << endl;
+    time_t timer;
+    struct tm *time_dead;
+    struct tm *time_now;
 
-                cout << "book rush removed" << endl;
-                cout << r2shs << endl;
-                JsonParser("../data/R2shs.json").Write(r2shs);
-            }
-        }
+    timer = time(NULL);
+    time_dead->tm_year = stoi(deadline.substr(0, 4)) - 1900;
+    cout << time_dead->tm_year << endl;
+    time_dead->tm_mon = stoi(deadline.substr(5, 2)) - 1;
+    cout << time_dead->tm_mon << endl;
+    time_dead->tm_mday = stoi(deadline.substr(8, 2));
+    cout << time_dead->tm_mday << endl;
+
+    time_now = localtime(&timer);
+    cout << deadline << endl;
+    time_t t1 = mktime(time_dead);
+    time_t t2 = mktime(time_now);
+    double diff = difftime(t1, t2); // If positive, then tm1 > tm2
+    if (diff > 0)
+    {
+        cout << diff << endl;
+        return "0000-00-00";
     }
     else
     {
-        cout << "book is not exist" << endl;
-        //없는 책이라고 알려주기
+        string res = to_string(time_dead->tm_year + 1900) + "-" + to_string(time_dead->tm_mon + 1) + "-" + to_string(time_dead->tm_mday);
+        return res;
     }
-    cout << r2shs[0]["rBId"] << endl;
+    return "";
+}
+void return_book(string rBId, Json::Value r2shs)
+{
+    int index = find_book(rBId, r2shs);
+    if (index == -1)
+    {
+        cout << "book not found" << endl;
+        return;
+    }
+    else
+    {
+        if (r2shs[index]["rDeadline"].asString() < get_today())
+        {
+            cout << "penalty occured" << endl;
+            Json::Value users = JsonParser("../data/Users.json").Read("users");
+            int userIndex = -1;
+            for (int i = 0; i < users.size(); i++)
+            {
+                if (users[i]["uId"] == r2shs[index]["rUid"])
+                {
+                    userIndex = i;
+                    cout << i << endl;
+                }
+            }
+            if (userIndex != -1)
+            {
+                users[userIndex]["uPenalty"] = get_penalty_date(r2shs[index]["rDeadline"].asString());
+                cout << users << endl;
+                // JsonParser("../data/Users.json").Write(users);
+            }
+            else
+            {
+                cout << "user not found" << endl;
+            }
+            // user 에 정보 저장 (penalty)
+        }
+        cout << "return book" << endl;
+        r2shs[index]["rDate"] = get_today();
+        //연체여부판단.
+        cout << "book returned" << endl;
+        cout << "book name: " << r2shs[index]["rUid"] << "rDate:" << r2shs[index]["rDate"] << endl;
+
+        JsonParser("../data/R2shs.json").Write(r2shs);
+    }
+    // cout << r2shs[0]["rBId"] << endl;
 }
 int main(int args, char **argv)
 {
-    Json::Value r2shs = JsonParser("../data/R2shs.json").Read();
+    Json::Value r2shs = JsonParser("../data/R2shs.json").Read("r2shs");
     // cout << r2shs[0]["rName"].asString().c_str() << endl;
     cout << argv[1] << endl;
     return_book(argv[1], r2shs);
