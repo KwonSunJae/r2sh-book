68299825789b2936836a2684584fad891fb3a50d
KwonSunJae <32411719+KwonSunJae@users.noreply.github.com>
2022-10-21 21:58:56 +0900

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,30 +2,30 @@
 
 case "$1" in
                 show)
-                        bash ~/r2sh-book/build/show "$@";
+                        ~/r2sh-book/build/show "$@";
                         ;;
                 search)
-                        bash ~/r2sh-book/build/search  "$@";
+                           ~/r2sh-book/build/search  "$@";
                         ;;
                 loan)
-                        bash ~/r2sh-book/build/loan  "$@";
+                           ~/r2sh-book/build/loan  "$@";
                         ;;
                 return)
-                        bash ~/r2sh-book/build/return  "$@";
+                           ~/r2sh-book/build/return  "$@";
                         ;;
                 info)
-                        bash ~/r2sh-book/build/info  "$@";
+                           ~/r2sh-book/build/info  "$@";
                         ;;
 
                 auth)
-                        bash ~/r2sh-book/build/auth  "$@";
+                           ~/r2sh-book/build/auth  "$@";
 
                         ;;
                 help)
-                        bash ~/r2sh-book/build/help  "$@";
+                           ~/r2sh-book/build/help  "$@";
                         ;;
                 *)
                         echo "$@" "is wrong commands!";
-                        bash ~/r2sh-book/build/help  "$@";
+                           ~/r2sh-book/build/help  "$@";
                         ;;
 esac
