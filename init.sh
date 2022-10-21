44e3ecbea928f25a47b88e29ec0eb78cba0da4e4
KwonSunJae <32411719+KwonSunJae@users.noreply.github.com>
2022-10-21 22:10:52 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -44,9 +44,10 @@ g++ -o ../build/info info.cpp jsoncpp.cpp -I. -DJSON_IS_AMALGAMATION
 g++ -o ../build/search search.cpp jsoncpp.cpp -I. -DJSON_IS_AMALGAMATION
 g++ -o ../build/loan loan.cpp jsoncpp.cpp -I. -DJSON_IS_AMALGAMATION
 g++ -o ../build/return return.cpp jsoncpp.cpp -I. -DJSON_IS_AMALGAMATION
-g++ -o ../build/info info.cpp jsoncpp.cpp -I. -DJSON_IS_AMALGAMATION
-g++ -o ../build/info info.cpp jsoncpp.cpp -I. -DJSON_IS_AMALGAMATION
+g++ -o ../build/auth auth.cpp jsoncpp.cpp -I. -DJSON_IS_AMALGAMATION
 g++ -o ../build/check check.cpp jsoncpp.cpp -I. -DJSON_IS_AMALGAMATION
+g++ -o ../build/show show.cpp jsoncpp.cpp -I. -DJSON_IS_AMALGAMATION
+
 cd ~/r2sh-book
 chmod 777 build/*
 echo "----COMPLETE-----"
