cbfcd436cf9b924f3900bec71355ef17ff489ea4
KwonSunJae <32411719+KwonSunJae@users.noreply.github.com>
2022-10-21 21:41:04 +0900

    [SHELL] update main.sh

--- init.sh
+++ init.sh
@@ -17,14 +17,20 @@ spin &
 SPIN_PID=$!
 trap "kill -9 $SPIN_PID" `seq 0 15`
 echo "-----Initalizing r2sh-book-----"
+if (( $EUID == 0 )); then
+  echo "root is very dangerous. Please change your Account"
+  echo $EUID
+  exit
+fi
+
+echo $EUID
+cd ~/r2sh-book
 echo "-----Install essential packages-----"
 
-sudo apt-get update -y -q
-sudo apt-get install gcc gcc-9 g++ -y -q
+sudo apt-get update -y -qq
+sudo apt-get install gcc gcc-9 g++ -y -qq
 gcc --version
-sudo apt-get install git -y -q
-sudo apt-get install python3 -y -q
-
+sudo apt-get install git -y -qq
 echo "-----Alias Settings-----"
 chmod 777 main.sh
 
@@ -33,13 +39,6 @@ alias r2sh-book='bash ~/r2sh-book/main.sh "\$@"'
 EOF
 source ~/.bashrc
 echo "----- BUILD -----"
-git clone https://github.com/open-source-parsers/jsoncpp.git
-
-cd jsoncpp
-
-python3 ~/r2sh-book/jsoncpp/amalgamate.py
-cd ~/r2sh-book
-cp -r jsoncpp/dist/* src
 cd src
 g++ -o ../build/info info.cpp jsoncpp.cpp -I. -DJSON_IS_AMALGAMATION
 g++ -o ../build/search search.cpp jsoncpp.cpp -I. -DJSON_IS_AMALGAMATION
@@ -48,7 +47,7 @@ g++ -o ../build/return return.cpp jsoncpp.cpp -I. -DJSON_IS_AMALGAMATION
 g++ -o ../build/info info.cpp jsoncpp.cpp -I. -DJSON_IS_AMALGAMATION
 g++ -o ../build/info info.cpp jsoncpp.cpp -I. -DJSON_IS_AMALGAMATION
 g++ -o ../build/check check.cpp jsoncpp.cpp -I. -DJSON_IS_AMALGAMATION
-
+cd ~/r2sh-book
 chmod 777 build/*
 echo "----COMPLETE-----"
 kill -9 $SPIN_PID
--- main.sh
+++ main.sh
@@ -1,3 +1,31 @@
 #! /bin/bash
 
-echo "$@"
+case "$1" in
+                show)
+                        bash /home/ubuntu/r2sh-book/src/show.sh "$@";
+                        ;;
+                search)
+                        bash /home/ubuntu/r2sh-book/src/search.sh "$@";
+                        ;;
+                loan)
+                        bash /home/ubuntu/r2sh-book/src/loan.sh "$@";
+                        ;;
+                return)
+                        bash /home/ubuntu/r2sh-book/src/return.sh "$@";
+                        ;;
+                info)
+                        bash /home/ubuntu/r2sh-book/src/info.sh "$@";
+                        ;;
+
+                auth)
+                        bash /home/ubuntu/r2sh-book/src/auth.sh "$@";
+
+                        ;;
+                help)
+                        bash /home/ubuntu/r2sh-book/src/help.sh "$@";
+                        ;;
+                *)
+                        echo "$@" "is wrong commands!";
+                        bash /home/ubuntu/r2sh-book/src/help.sh "$@";
+                        ;;
+esac
