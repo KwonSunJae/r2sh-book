c3418bfdcbc3f4222c8b2a41bbb6f338d8f1b702
binllionaire <46593078+binllionaire@users.noreply.github.com>
2022-10-22 23:28:04 +0900

    check edit

--- src/check.cpp
+++ src/check.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <ctype.h>
 #include <fstream>
 #include <string>
 #include <sstream>
 #include <unistd.h>
 #include <limits.h>
+#include <stdlib.h>
 #include "json/json.h" //라이브러리 헤더파일
 using namespace std;
 
@@ -19,6 +21,23 @@ public:
     JsonParserUser()
     {
         filedata = "../data/Users.json"; //대상 파일임
+    }
+
+    bool checkTab(string str)
+    {
+        int flag = 0;
+        for (int i = 0; i < (int)str.length(); ++i)
+        {
+
+            if (!(isspace(str[i]) == 0) || (str.find("\b") != string::npos))
+            {
+                flag++;
+            }
+        }
+        if (flag > 0)
+            return false;
+        else
+            return true;
     }
 
     int checkDate(int year, int month, int day)
@@ -73,32 +92,37 @@ public:
         {
             for (int i = 0; i < root["users"].size(); i++)
             {
-                //cout<<"big "<<endl;
+
+                // cout<<"big "<<endl;
                 if ((root["users"][i].isMember("uId")) && (root["users"][i].isMember("uName")) && (root["users"][i].isMember("uPenalty")) && (root["users"][i].isMember("uR2shs")))
                 {
-                    //cout<<"1 "<<endl;
+                    if (!(checkTab((root["users"][i]["uId"]).asString()) == true))
+                        exit(EXIT_FAILURE); // uId 개행문자 있으면
+                    if (!(checkTab((root["users"][i]["uName"]).asString()) == true))
+                        exit(EXIT_FAILURE); // uName 개행문자 있으면
+                    // cout<<"1 "<<endl;
                     if (!root["users"][i]["uId"].isString())
                         exit(EXIT_FAILURE);
-                    //cout<<"2 "<<endl;    
+                    // cout<<"2 "<<endl;
                     if (root["users"][i]["uId"].asString().length() != 6)
                         exit(EXIT_FAILURE);
-                    //cout<<"3 "<<endl;
+                    // cout<<"3 "<<endl;
                     if (!root["users"][i]["uName"].isString())
                         exit(EXIT_FAILURE);
-                    //cout<<"4 "<<endl;
+                    // cout<<"4 "<<endl;
                     if (root["users"][i]["uName"].asString().length() > 20)
                         exit(EXIT_FAILURE);
-                    //cout<<"5 "<<endl;
+                    // cout<<"5 "<<endl;
                     if (root["users"][i]["uR2shs"].size() > 5)
                         exit(EXIT_FAILURE);
-                    //cout<<"6 "<<endl;    
+                    // cout<<"6 "<<endl;
                     for (int su = 0; su < root["users"][i]["uR2shs"].size(); su++)
                     {
                         if (!root["users"][i]["uR2shs"][su].isInt())
                             exit(EXIT_FAILURE);
                     }
 
-                    //cout<<"7 "<<endl;
+                    // cout<<"7 "<<endl;
                     if (!root["users"][i]["uPenalty"].isString())
                         exit(EXIT_FAILURE);
                     string date = root["users"][i]["uPenalty"].asString();
@@ -107,11 +131,11 @@ public:
                     stringstream ss(date);
                     getline(ss, token, '-');
                     stringstream ssInt(token);
-                    //cout<<date << token <<endl;
+                    // cout<<date << token <<endl;
                     if (token.length() != 4)
                         exit(EXIT_FAILURE);
                     ssInt >> y;
-                    //cout<<"big "<<endl;
+                    // cout<<"big "<<endl;
                     if (ssInt.fail())
                         exit(EXIT_FAILURE);
                     getline(ss, token, '-');
@@ -128,8 +152,8 @@ public:
                     ssIntd >> d;
                     if (ssIntd.fail())
                         exit(EXIT_FAILURE);
-                    //cout<<"checkDate "<<endl;
-                    if (date.compare("0000-00-00")!=0&&!checkDate(y, m, d))
+                    // cout<<"checkDate "<<endl;
+                    if (date.compare("0000-00-00") != 0 && !checkDate(y, m, d))
                         exit(EXIT_FAILURE);
 
                     continue;
@@ -151,6 +175,48 @@ public:
         filedata = "../data/Books.json"; //대상 파일임
     }
 
+    bool checkSpace(string str)
+    {
+        int flag = 0;
+        for (int i = 0; i < (int)str.length(); ++i)
+        {
+
+            if (str[i] == ' ')
+            {
+            }
+            else if (!(isspace(str[i]) == 0) || (str.find("\b") != string::npos))
+            {
+
+                flag++;
+            }
+        }
+        if (flag > 0)
+            return false;
+        else
+            return true;
+    }
+
+    bool checkTab(string str)
+    {
+        int flag = 0;
+
+        for (int i = 0; i < str.length(); ++i)
+        {
+            if (!(isspace(str[i]) == 0) || (str.find("\b") != string::npos))
+            {
+
+                flag++;
+            }
+        }
+        if (flag > 0)
+        {
+            exit(EXIT_FAILURE);
+            return false;
+        }
+
+        else
+            return true;
+    }
     void nameChecker(Json::Value data)
     {
         string name = data.asString();
@@ -222,6 +288,18 @@ public:
             authorsChecker(root["books"][i]["bAuthors"]);
             categorysChecker(root["books"][i]["bCategorys"]);
             detailChecker(root["books"][i]["bDetail"]);
+            checkSpace((root["books"][i]["bName"]).asString());
+
+            checkTab((root["books"][i]["bId"]).asString());
+
+            for (int j = 0; j < (root["books"][i]["bAuthors"]).size(); j++)
+            {
+                checkSpace((root["books"][i]["bAuthors"][j]).asString());
+            }
+            for (int j = 0; j < (root["books"][i]["bAuthors"]).size(); j++)
+            {
+                checkTab((root["books"][i]["bCategorys"][j]).asString());
+            }
         }
     }
 };
@@ -237,17 +315,38 @@ public:
     {
         filedata = "../data/R2shs.json"; //대상 파일임
     }
+    bool checkTab(string str)
+    {
+        int flag = 0;
+
+        for (int i = 0; i < (int)str.length(); ++i)
+        {
+
+            if (!(isspace(str[i]) == 0) || (str.find("\b") != string::npos))
+            {
+
+                flag++;
+            }
+            if (flag > 0)
+                return false;
+            else
+                return true;
+        }
+    }
     int checkDate(int year, int month, int day)
     {
         if ((month % 2 == 1)) // odd month
         {
-            if (month <= 7){
+            if (month <= 7)
+            {
                 if (day <= 0 || day > 31)
                     return false;
             }
             else // month > 7
-                {if (day <= 0 || day > 30)
-                        return false;}
+            {
+                if (day <= 0 || day > 30)
+                    return false;
+            }
         }
         else // even month
         {
@@ -293,18 +392,23 @@ public:
             for (int i = 0; i < root["r2shs"].size(); i++)
             {
                 //사이즈 확인
-                //cout<<"1 "<<endl;
+                // cout<<"1 "<<endl;
+
                 if ((root["r2shs"][i].isMember("rId")) && (root["r2shs"][i].isMember("rUid")) && (root["r2shs"][i].isMember("rBid")) && (root["r2shs"][i].isMember("rDate")) && (root["r2shs"][i].isMember("rDeadline")))
                 {
-                    
                 }
                 else
                     exit(EXIT_FAILURE);
-                //cout<<"2 "<<endl;
+                // cout<<"2 "<<endl;
                 if (!(root["r2shs"][i]["rId"]).isInt() || !(0 <= root["r2shs"][i]["rId"].asInt() && root["r2shs"][i]["rId"].asInt() <= INT_MAX) || !((root["r2shs"][i]["rUid"]).asString().length() == 6) || !((root["r2shs"][i]["rBid"]).asString().length() == 8))
                     exit(EXIT_FAILURE);
-
-                //cout<<"Date "<<endl;
+                if (checkTab((root["r2shs"][i]["rId"]).asString()) == false)
+                    exit(EXIT_FAILURE);
+                if (checkTab((root["r2shs"][i]["rUid"]).asString()) == false)
+                    exit(EXIT_FAILURE);
+                if (checkTab((root["r2shs"][i]["rBid"]).asString()) == false)
+                    exit(EXIT_FAILURE);
+                // cout<<"Date "<<endl;
                 if (!root["r2shs"][i]["rDate"].isString())
                     exit(EXIT_FAILURE);
                 string date = root["r2shs"][i]["rDate"].asString();
@@ -332,27 +436,25 @@ public:
                 ssIntd >> d;
                 if (ssIntd.fail())
                     exit(EXIT_FAILURE);
-                
-                if (date.compare("0000-00-00")!=0&&!checkDate(y, m, d))
+
+                if (date.compare("0000-00-00") != 0 && !checkDate(y, m, d))
                     exit(EXIT_FAILURE);
 
-
-
-                //cout<<"deadlin "<<endl;
+                // cout<<"deadlin "<<endl;
                 if (!root["r2shs"][i]["rDeadline"].isString())
                     exit(EXIT_FAILURE);
                 date = root["r2shs"][i]["rDeadline"].asString();
                 stringstream ss2(date);
                 getline(ss2, token, '-');
                 stringstream ssInts(token);
-                //cout<<"1 "<<endl;
+                // cout<<"1 "<<endl;
                 if (token.length() != 4)
                     exit(EXIT_FAILURE);
                 ssInts >> y;
                 if (ssInts.fail())
                     exit(EXIT_FAILURE);
 
-                //cout<<token <<endl;
+                // cout<<token <<endl;
                 getline(ss2, token, '-');
                 if (token.length() != 2)
                     exit(EXIT_FAILURE);
@@ -361,7 +463,7 @@ public:
                 if (ssIntms.fail())
                     exit(EXIT_FAILURE);
 
-                //cout<<token <<endl;
+                // cout<<token <<endl;
                 getline(ss2, token, '-');
                 if (token.length() != 2)
                     exit(EXIT_FAILURE);
@@ -369,8 +471,8 @@ public:
                 ssIntds >> d;
                 if (ssIntds.fail())
                     exit(EXIT_FAILURE);
-                //cout<<date <<"compare"<<y<<m<<d<<checkDate(y,m,d)<<endl;
-                if (date.compare("0000-00-00")!=0&&!checkDate(y, m, d))
+                // cout<<date <<"compare"<<y<<m<<d<<checkDate(y,m,d)<<endl;
+                if (date.compare("0000-00-00") != 0 && !checkDate(y, m, d))
                     exit(EXIT_FAILURE);
             }
         }
@@ -389,16 +491,16 @@ int main()
 
     if ((access(filePath1.c_str(), F_OK) == 0) && (access(filePath2.c_str(), F_OK) == 0) && (access(filePath3.c_str(), F_OK) == 0)) //
     {
-        //std::cout << "FILE EXIST" << std::endl;
+        // std::cout << "FILE EXIST" << std::endl;
         JPuser.Read();
-        //cout<<"jaemin"<<endl;
+        // cout<<"jaemin"<<endl;
         JPbook.Read();
-        //cout<<"subin"<<endl;
+        // cout<<"subin"<<endl;
         JPr2sh.Read();
     }
     else
     {
-       // std::cout << "INTEGRITY CHECK FAILED" << std::endl;
+        // std::cout << "INTEGRITY CHECK FAILED" << std::endl;
         exit(EXIT_FAILURE);
         // exit(1);
     }
