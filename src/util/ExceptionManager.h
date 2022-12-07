a11e7f18d6556edab035566c1898498293b1b3e7
woo4826 <woo4826@gmail.com>
2022-12-07 18:43:26 +0900

    [FIX] 오타수정

--- src/util/ExceptionManager.h
+++ src/util/ExceptionManager.h
@@ -6,18 +6,18 @@
 using namespace std;
 
 const string helpMessage =
-                 "==========================================\n"
-                 "R2SH-BOOK COMMANDS!!\n"
-                 "==========================================\n"
-                 "show <book-id> : show detail for the book\n"
-                 "search <keyword> : search book by keyword and show list\n"
-                 "search -c <category> : serach book by category and show list\n"
-                 "loan <user-id> : borrow book by bid\n"
-                 "return  <book-id> : return book by bid\n"
-                 "info <user-id> : show list of books borrowed by the user\n"
-                 "info <book-id>	: show you can borrow this book\n",
-             "auth <username> : generate unique user-id according to username\n"
-             "help : show available commandz list\n";
+    "==========================================\n"
+    "R2SH-BOOK COMMANDS!!\n"
+    "==========================================\n"
+    "show <book-id> : show detail for the book\n"
+    "search <keyword> : search book by keyword and show list\n"
+    "search -c <category> : serach book by category and show list\n"
+    "loan <user-id> : borrow book by bid\n"
+    "return  <book-id> : return book by bid\n"
+    "info <user-id> : show list of books borrowed by the user\n"
+    "info <book-id>	: show you can borrow this book\n"
+    "auth <username> : generate unique user-id according to username\n"
+    "help : show available commandz list\n";
 
 class ExceptionManager
 {
