74e1dce9d341a04c80d07f675ce785f7dcd030ed
Woojin Wie <dnldnwls1123@gmail.com>
2022-12-11 20:47:11 +0900

    Fix : errMsg

--- src/util/ExceptionManager.h
+++ src/util/ExceptionManager.h
@@ -6,18 +6,20 @@
 using namespace std;
 
 const string helpMessage =
-    "==========================================\n"
-    "R2SH-BOOK COMMANDS!!\n"
-    "==========================================\n"
-    "show <book-id> : show detail for the book\n"
-    "search <keyword> : search book by keyword and show list\n"
-    "search -c <category> : serach book by category and show list\n"
-    "loan <user-id> : borrow book by bid\n"
-    "return  <book-id> : return book by bid\n"
-    "info <user-id> : show list of books borrowed by the user\n"
-    "info <book-id>	: show you can borrow this book\n"
-    "auth <username> : generate unique user-id according to username\n"
-    "help : show available command list\n";
+"------------------------------------------------------------------------------\n"
+"R2SH-BOOK COMMANDS!!\n"
+"------------------------------------------------------------------------------\n"
+"show  <book-id> : show detail for the book\n"
+"search  <keyword> : serach book by keyword and show list\n"
+"search -c <category> : serach book by category and show list\n"
+"loan <user-id> <book-id> : borrow book by bid\n"
+"return <book-id> : return book by bid\n"
+"info <user-id>	: show list of books borrowed by the user\n"
+"info <book-id>	: show you can borrow this book\n"
+"auth <username> : generate unique user-id according to username\n"
+"find <username> : find your userID by username\n"
+"help : show available command list\n"
+"------------------------------------------------------------------------------\n";
 
 class ExceptionManager
 {
